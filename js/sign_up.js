e7e66b024dd581df65281a023d745b62f09125c9
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-05 20:52:28 +0300

    signin

--- js/sign_up.js
+++ js/sign_up.js
@@ -6,67 +6,55 @@ function checkEmpty() {
     var birthdate = document.getElementById("birthdate").value;
     var password = document.getElementsByName("password_sign_up")[0].value;
     return email != "" & name != null & surname != null & birthdate != "" & password != "";
+}
 
+document.getElementById('sign_in').addEventListener('click', (e) => {
+  e.preventDefault();
+  var email = document.getElementsByName("email_address_sign_in")[0].value;
+  var password = document.getElementsByName("password_sign_in")[0].value;
+
+
+  if (localStorage.getItem(email) === null) {
+    console.log("test");
+    document.getElementById("sign_in_error").innerHTML = "user does not exist or wrong email";
   }
 
-  function signIn() {
-
-    var email = document.getElementsByName("email_address_sign_in")[0].value;
-    var password = document.getElementsByName("password_sign_in")[0].value;
-
-    if (localStorage) {
-
-      if (localStorage.getItem(email) == null) {
-        document.getElementById("sign_in_error").innerHTML = "user does not exist or wrong email";
-        return false;
-      }
-
-      else if (localStorage.getItem(email) == password) {
-        sessionStorage.setItem("sign_in", true);
-        return true;
-      }
-
-      else {
-        document.getElementById("sign_in_error").innerHTML = "wrong password";
-        return false;
-      }
-
-    }
-
-    else {
-      alert("Sorry, your browser does not support local storage");
-    }
-
+  else if (localStorage.getItem(email) == password) {
+    sessionStorage.setItem("sign_in", true);
+    window.location.href="index.html";
   }
 
-  function signUp() {
+  else {
+    document.getElementById("sign_in_error").innerHTML = "wrong password";
+  }
+});
 
-    var email = document.getElementsByName("email_address_sign_up")[0].value;
-    var password = document.getElementsByName("password_sign_up")[0].value;
 
-    if (localStorage) {
+document.getElementById('sign_up').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log("signUp");
+  var email = document.getElementsByName("email_address_sign_up")[0].value;
+  var password = document.getElementsByName("password_sign_up")[0].value;
 
-        if (localStorage.getItem(email) == null & checkEmpty()) {
-        localStorage.setItem(email, password);
-        return true;
-      }
-
-      else if (!checkEmpty()) {
-        document.getElementById("sign_up_error").innerHTML = "please fill in each field";
-        return false;
-      }
-
-      else {
-        document.getElementById("sign_up_error").innerHTML = "user already exists";
-        return false;
-      }
-
+  if (localStorage) {
+      if (localStorage.getItem(email) == null & checkEmpty()) {
+      localStorage.setItem(email, password);
+      return true;
     }
 
-    else {
-      alert("Sorry, your browser does not support local storage");
+    else if (!checkEmpty()) {
+      document.getElementById("sign_up_error").innerHTML = "please fill in each field";
       return false;
     }
 
+    else {
+      document.getElementById("sign_up_error").innerHTML = "user already exists";
+      return false;
+    }
   }
-  
+
+  else {
+    alert("Sorry, your browser does not support local storage");
+    return false;
+  }
+})
